883d83fd77412f7aedc219068803abe4b2790a80
kadish-apiiro <david.kadish@apiiro.com>
2025-02-19 16:40:10 +0200

    Update new_google.py

--- new_google.py
+++ new_google.py
@@ -1,5 +1,7 @@
 from google import genai
-client = genai.Client(api_key='GEMINI_API_KEY')
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is the sky blue?'
-)
+
+def execute():
+    client = genai.Client(api_key='GEMINI_API_KEY')
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+    )
